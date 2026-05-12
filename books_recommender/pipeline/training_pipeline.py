aefbf6249abb5abbb9f69850ff624a41d174db0c
vivekta008 <tavivek256@gmail.com>
2026-05-12 09:48:00 +0530

    Data transformation stage completed successfully. The transformed data has been saved to the specified location. The next step is to proceed with model training using the transformed data. Please ensure that all necessary configurations for model training are set correctly before starting the training pipeline.

--- books_recommender/pipeline/training_pipeline.py
+++ books_recommender/pipeline/training_pipeline.py
@@ -1,5 +1,6 @@
 from books_recommender.components.stage_00_data_ingestion import DataIngestion
 from books_recommender.components.stage_01_data_validation import DataValidation
+from books_recommender.components.stage_02_data_transformation import DataTransformation
 
 
 
@@ -8,6 +9,7 @@ class TrainingPipeline:
     def __init__(self):
         self.data_ingestion = DataIngestion()
         self.data_validation = DataValidation()
+        self.data_transformation = DataTransformation()
 
 
     def start_training_pipeline(self):
@@ -17,3 +19,4 @@ class TrainingPipeline:
         """
         self.data_ingestion.initiate_data_ingestion()
         self.data_validation.initiate_data_validation()
+        self.data_transformation.initiate_data_transformation()
