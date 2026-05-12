32acb69ecfb0690f88049fff2fd0e275a6cfe102
vivekta008 <tavivek256@gmail.com>
2026-05-12 09:58:23 +0530

    trained model and updated the training pipeline to include the new model. Also updated the configuration files to reflect the changes in the model training process.

--- books_recommender/pipeline/training_pipeline.py
+++ books_recommender/pipeline/training_pipeline.py
@@ -1,6 +1,7 @@
 from books_recommender.components.stage_00_data_ingestion import DataIngestion
 from books_recommender.components.stage_01_data_validation import DataValidation
 from books_recommender.components.stage_02_data_transformation import DataTransformation
+from books_recommender.components.stage_03_model_trainer import ModelTrainer
 
 
 
@@ -10,6 +11,7 @@ class TrainingPipeline:
         self.data_ingestion = DataIngestion()
         self.data_validation = DataValidation()
         self.data_transformation = DataTransformation()
+        self.model_trainer = ModelTrainer()
 
 
     def start_training_pipeline(self):
@@ -20,3 +22,4 @@ class TrainingPipeline:
         self.data_ingestion.initiate_data_ingestion()
         self.data_validation.initiate_data_validation()
         self.data_transformation.initiate_data_transformation()
+        self.model_trainer.initiate_model_trainer()
